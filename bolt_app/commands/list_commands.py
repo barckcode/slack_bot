7ae1dff3a289d79c7f20e10e84267fbb8cfdb339
BarckCode <barckcode@gmail.com>
2021-08-10 19:38:34 +0200

    Completed /help command

--- bolt_app/commands/list_commands.py
+++ bolt_app/commands/list_commands.py
@@ -1,14 +1,56 @@
 # List of commands
 def init_commands(app):
+    @app.command("/help")
+    def help_command(ack, say, command):
+        # Acknowledge command request
+        ack()
+        say(
+            blocks=[
+                {
+                    "type": "divider",
+                },
+                {
+                    "type": "header",
+                    "text": {
+                        "type": "plain_text",
+                        "text": "Comandos de R2D2"
+                    }
+                },
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": f"*Hola <@{command['user_name']}>*, te dejo los comandos que tengo disponibles:"
+                    },
+                },
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": "- */echo* [text]: Comando que responde con lo que hayas escrito"
+                    }
+                },
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": "- */rollback* [application]: Comando para dar marcha atrás tras un deploy en PROD"
+                    }
+                },
+            ],
+            text=f"Hola <@{command['user_name']}>!"
+        )
+
+
     @app.command("/echo")
-    def repeat_text(ack, say, command):
+    def echo_command(ack, say, command):
         # Acknowledge command request
         ack()
         say(f"{command['text']}")
 
 
-    @app.command("/help")
-    def repeat_text(ack, say, command):
+    @app.command("/rollback")
+    def rollback_command(ack, say, command):
         # Acknowledge command request
         ack()
         say(
@@ -20,9 +62,9 @@ def init_commands(app):
                     "type": "section",
                     "text": {
                         "type": "mrkdwn",
-                        "text": f"*Hola <@{command['user_name']}>!*, te dejo los comandos que tengo disponibles:"
+                        "text": f"{command['text']}"
                     },
                 },
             ],
-            text=f"Hey there <@{command['user_name']}>!"
+            text=f"Rollback <@{command['text']}>!"
         )
